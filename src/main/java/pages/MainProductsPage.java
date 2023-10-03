b470ec4ebb34854346f73d5e81efef08d05d583f
suvoda/iprankevich <iprankevich@suvoda.com>
2023-10-03 15:58:05 +0300

    test

--- src/main/java/pages/MainProductsPage.java
+++ src/main/java/pages/MainProductsPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
 import static org.testng.Assert.*;
 import static utils.Utils.extractDouble;
 
@@ -27,7 +28,7 @@ public class MainProductsPage extends BasePage {
         super(driver);
     }
 
-    public void verifyMainProductsPageUiElements(){
+    public void verifyMainProductsPageUiElements() {
         assertTrue(find(By.className("app_logo")).getText().equals("Swag Labs"));
         assertTrue(find(By.className("title")).getText().equals("Products"));
         assertTrue(find(By.id("react-burger-menu-btn")).isDisplayed());
@@ -35,75 +36,78 @@ public class MainProductsPage extends BasePage {
         assertTrue(find(By.className("product_sort_container")).isDisplayed());
     }
 
-    public Boolean isProductDisplayed(String productName){
-            return isDisplayed(By.xpath(String.format(PRODUCT_XPATH, productName)));
+    public Boolean isProductDisplayed(String productName) {
+        return isDisplayed(By.xpath(String.format(PRODUCT_XPATH, productName)));
     }
 
-    public void clickOnAddToCartButtonForItem(String productName){
+    public void clickOnAddToCartButtonForItem(String productName) {
+        isProductDisplayed(productName);
         click(By.xpath(String.format(ADD_TO_CART_BUTTON_XPATH, productName)));
         verifyThatRemoveButtonIsDisplayed(productName);
     }
 
-    public void clickOnRemoveButtonForItem(String productName){
+    public void clickOnRemoveButtonForItem(String productName) {
+        isProductDisplayed(productName);
         click(By.xpath(String.format(REMOVE_BUTTON_XPATH, productName)));
         verifyThatAddToCartButtonIsDisplayed(productName);
     }
 
-    public void verifyThatRemoveButtonIsDisplayed(String productName){
-        assertTrue(isDisplayed(By.xpath(String.format(REMOVE_BUTTON_XPATH,productName))));
+    public void verifyThatRemoveButtonIsDisplayed(String productName) {
+        assertTrue(isDisplayed(By.xpath(String.format(REMOVE_BUTTON_XPATH, productName))));
     }
 
-    public void verifyThatAddToCartButtonIsDisplayed(String productName){
-        assertTrue(isDisplayed(By.xpath(String.format(ADD_TO_CART_BUTTON_XPATH,productName))));
+    public void verifyThatAddToCartButtonIsDisplayed(String productName) {
+        assertTrue(isDisplayed(By.xpath(String.format(ADD_TO_CART_BUTTON_XPATH, productName))));
     }
-    public Double getProductPrice(String productName){
+
+    public Double getProductPrice(String productName) {
         String priceString = find(By.xpath(String.format(PRICE_XPATH, productName))).getText();
         Double price = extractDouble(priceString);
         return price;
     }
 
-    public String getCartQuantityValue(){
+    public String getCartQuantityValue() {
         return find(shopping_cart_container).getText();
     }
 
-    public void verifyCartBadge(String expectedQuantity){
+    public void verifyCartBadge(String expectedQuantity) {
         assertTrue(getCartQuantityValue().equals(expectedQuantity));
     }
 
-    public void verifyThatCartIsEmpty(){
+    public void verifyThatCartIsEmpty() {
         assertTrue(getCartQuantityValue().isEmpty());
     }
 
-    public CartPage clickOnCart(){
+    public CartPage clickOnCart() {
         find(shopping_cart_container).isDisplayed();
         click(shopping_cart_container);
         return new CartPage(driver);
     }
 
-    public BurgerMenu clickOnBurgerMenu()  {
+    public BurgerMenu clickOnBurgerMenu() {
         find(burgerMenuButton).isDisplayed();
         click(burgerMenuButton);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
         return new BurgerMenu(driver);
     }
 
-    public void logout (){
+    public void logout() {
         BurgerMenu burgerMenu = clickOnBurgerMenu();
         burgerMenu.clickOnLogoutButton();
     }
 
-    public ProductPage clickOnProduct (String productName){
-        find(By.xpath(String.format(PRODUCT_LINK_XPATH,productName))).isDisplayed();
-        click(By.xpath(String.format(PRODUCT_LINK_XPATH,productName)));
+    public ProductPage clickOnProduct(String productName) {
+        find(By.xpath(String.format(PRODUCT_LINK_XPATH, productName))).isDisplayed();
+        click(By.xpath(String.format(PRODUCT_LINK_XPATH, productName)));
         return new ProductPage(driver);
     }
 
-    public void clickOnOrdering(){
+    public void clickOnOrdering() {
         click(sortContainer);
     }
 
-    public void clickOnOrderingOption(String option){
+    public void clickOnOrderingOption(String option) {
         clickOnOrdering();
-        click(By.xpath(String.format(SORT_OPTION,option)));
+        click(By.xpath(String.format(SORT_OPTION, option)));
     }
 }
