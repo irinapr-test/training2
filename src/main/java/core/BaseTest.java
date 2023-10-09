9395bd9d85152cbbd52c34b8c4d0edb653ae3dee
suvoda/iprankevich <iprankevich@suvoda.com>
2023-10-09 14:33:54 +0300

    changes + new crossBrowser test

--- src/main/java/core/BaseTest.java
+++ src/main/java/core/BaseTest.java
@@ -3,25 +3,24 @@ package core;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import pages.LoginPage;
 
 import static utils.UIPropertiesLoader.getShopURL;
 
 
 public class BaseTest {
-    // TODO let's think about transfering this address into a resource file.
     private static final String SHOP_URL = getShopURL();
     public static ChromeOptions options;
     public static WebDriver driver;
-
-
     protected LoginPage loginPage;
 
-    //TODO let's see if we can find a solution for chromedriver to be included in the project
-    //     either way, it should have a relative path to the project, not something with c:\\users\\ etc..
-    @BeforeClass
+     @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         options = new ChromeOptions();
@@ -31,7 +30,6 @@ public class BaseTest {
         loginPage = new LoginPage(driver);
     }
 
-    //TODO this should be public, that's why it doesn't work. And I think we should also remove static
     @AfterClass
     public void quitBrowser() {
         driver.quit();
