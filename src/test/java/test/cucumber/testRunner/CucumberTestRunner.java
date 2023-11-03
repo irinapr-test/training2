9b2d6a3868791eaac81e3e425bf85be2d049b33b
suvoda/iprankevich <iprankevich@suvoda.com>
2023-11-03 15:57:02 +0300

    cucumber

--- src/test/java/test/cucumber/testRunner/CucumberTestRunner.java
+++ src/test/java/test/cucumber/testRunner/CucumberTestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features="src/test/java/test/cucumber/featureFile",
-        glue={"stepDefinitions"}
+        glue={"test.cucumber.stepDefinitions"}
        )
 public class CucumberTestRunner extends AbstractTestNGCucumberTests {
 
